a2830f468023808d62f17ec2bc8abf11449929de
Raktim Mukhopadhyay <rmj.3197@gmail.com>
2025-09-28 18:17:56 -0400

    Update .gitignore and increase sleep in scrape_dma.py
    Added support for ignoring .xlsx files in the docs directory. Increased the sleep duration from 10 to 30 seconds in scrape_dma.py to potentially address timing or loading issues.

--- SurVigilance/ui/scrapers/scrape_dma.py
+++ SurVigilance/ui/scrapers/scrape_dma.py
@@ -142,7 +142,7 @@ def scrape_dma_sb(
                 pass
             step()
 
-            sb.sleep(10)
+            sb.sleep(30)
 
             outer_iframe = (
                 'iframe[src*="/upload/dap/dap.html?drug=./DK_EXTERNAL/NONCOMBINED/"]'
