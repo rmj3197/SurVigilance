8bf5337feaf25e1b327d2b9ed7136b6eda2422d4
Raktim Mukhopadhyay <raktimmu@buffalo.edu>
2026-09-08 13:26:37 -0400

    Update dma_example.py

--- docs/source/user_guide/dma_example.py
+++ docs/source/user_guide/dma_example.py
@@ -7,7 +7,7 @@ def main():
     out_dir = "dma_out"
     os.makedirs(out_dir, exist_ok=True)
 
-    med = "paracetamol"
+    med = "paracetamolum"
 
     df = scrape_dma_sb(medicine=med, output_dir=out_dir, headless=True)
     print(df.head())
